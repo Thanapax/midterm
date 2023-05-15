28d2cc2b304b5f95d1a9fa3a2feea7f739f7f6a0
Thanapax <93991244+Thanapax@users.noreply.github.com>
2023-05-15 16:58:51 +0700

    Update Program.cs

--- Idia-Camp/Program.cs
+++ Idia-Camp/Program.cs
@@ -1,33 +1,37 @@
-﻿class program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("pls Input all town: ");
-        int all = int.Parse(Console.ReadLine());
-        while(true){
-            Console.WriteLine("pls Input : ");
-            string town = Console.ReadLine();
+class program {
+    static void Main(string[] args){
+        Console.Write("V max : ");
+        double vMax = double.Parse(Console.ReadLine());
+        Console.Write("V drink : ");
+        double vdrink = double.Parse(Console.ReadLine());
+        Console.Write("V fill : ");
+        double vfill = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("pls Input value: ");
-            int value = int.Parse(Console.ReadLine());
+        Console.Write("T drink : ");
+        double tdrink = double.Parse(Console.ReadLine());
+        Console.Write("T fill : ");
+        double tfill = double.Parse(Console.ReadLine());
+        Console.Write("T day : ");
+        double tday = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("pls Input num: ");
-            int[] number = new int[value];
-                for (int i = 1; i <= value; i++)
-                {
-                    number[i] = int.Parse(Console.ReadLine());
-                }
+
+
+        if(vdrink <= vMax){
+            if(tday>= tdrink && tday >= tfill){
                 
-            
-        } 
-
-        
-
-
-
-
-
-
-
+                double a = (double)(tday/tdrink) ;
+                double b = (double)(vMax- vdrink);
+                double c = (double)(vMax-((b+vfill)-vdrink));
+                int fill = (int)(tday/tfill);
+                if(vMax >= a && b > 0){
+                    Console.WriteLine("Enough Water, {0} left" , c);
+                } else if(b - vdrink < 0) {
+                    Console.WriteLine("Not Enough Water");
+                }
+                if(c > 0){
+                    Console.WriteLine("Overflow Water");
+                }
+            }
+        }
     }
 }
